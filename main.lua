706f071baf20ad748f0becc19c32ac623a2457fb
PixelwarStudio <janko.matthes@gmx.net>
2016-09-27 17:26:20 +0200

    Set SUIT theme

--- main.lua
+++ main.lua
@@ -1,7 +1,7 @@
 -- Modules
 local Class = require('lib.middleclass')
 local Timer = require('lib.timer')
-local Gui = require('lib.suit')
+local Suit = require('lib.suit')
 
 -- Classes
 local Field, Rule, Cell, GameOfLife = require('gameOfLife')()
@@ -13,3 +13,11 @@ local rule = Rule({2, 3}, {3})
 local field = Field(50, 50)
 local gameOfLife = GameOfLife(field, rule)
 local isRunning = false
+
+-- Gui theme
+Suit.theme.cornerRadius = 0
+Suit.theme.color = {
+    normal  = {bg = {218, 40, 40}, fg = {0, 0, 0}},
+    hovered = {bg = {218, 22, 22}, fg = {0, 0, 0}},
+    active  = {bg = {218, 6, 6}, fg = {0, 0, 0}}
+}
